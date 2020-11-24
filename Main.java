fa9d83240e34a7d475fe0a410148363464c1a63a
oliveraton1234 <61020652+oliveraton1234@users.noreply.github.com>
2020-11-24 02:36:49 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+public class Main {
+    public static void main(String[] args) {
+    Lista lista = new Lista();
+
+    for(int i = 0; i < 1000001; ++i){
+        Random R = new Random();
+        int x = R.nextInt(100001);
+
+        lista.insertar(x);
+    }
+
+    lista.shellSort();
+    lista.recorrer();
+
+    }
